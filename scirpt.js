91192b640ecd085588610a1fe6224f0fbd0d264d
menaa2637489-star <menaa2637489@gmail.com>
2026-09-15 15:06:41 +0300

    Update scirpt.js

--- scirpt.js
+++ scirpt.js
@@ -1,1 +1,66 @@
+/* =========================
+   LANGUAGE
+========================= */
 
+const languageBtn = document.getElementById("languageBtn");
+
+let currentLanguage = "ar";
+
+languageBtn.addEventListener("click", function () {
+
+    if (currentLanguage === "ar") {
+
+        currentLanguage = "en";
+
+        document.documentElement.lang = "en";
+        document.documentElement.dir = "ltr";
+
+        languageBtn.textContent = "العربية";
+
+    } else {
+
+        currentLanguage = "ar";
+
+        document.documentElement.lang = "ar";
+        document.documentElement.dir = "rtl";
+
+        languageBtn.textContent = "English";
+    }
+
+    const elements = document.querySelectorAll("[data-ar][data-en]");
+
+    elements.forEach(function (element) {
+
+        element.textContent =
+            currentLanguage === "ar"
+                ? element.getAttribute("data-ar")
+                : element.getAttribute("data-en");
+
+    });
+
+});
+
+
+/* =========================
+   DISCOVER PRODUCTS
+========================= */
+
+const heroProductsBtn =
+    document.querySelector(".hero-products-btn");
+
+if (heroProductsBtn) {
+
+    heroProductsBtn.addEventListener("click", function () {
+
+        const productsSection =
+            document.getElementById("products");
+
+        if (productsSection) {
+            productsSection.scrollIntoView({
+                behavior: "smooth"
+            });
+        }
+
+    });
+
+}
