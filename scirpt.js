61c4d3ee2c46947ea3d4cfa8f9f23e172ce844a2
menaa2637489-star <menaa2637489@gmail.com>
2026-09-16 11:58:31 +0300

    Update scirpt.js

--- scirpt.js
+++ scirpt.js
@@ -6,6 +6,83 @@ const languageBtn = document.getElementById("languageBtn");
 
 let currentLanguage = "ar";
 
+function updateLanguage() {
+
+    /* =========================
+       CHANGE TEXT
+    ========================= */
+
+    const elements =
+        document.querySelectorAll("[data-ar][data-en]");
+
+    elements.forEach(function (element) {
+
+        if (currentLanguage === "en") {
+
+            element.textContent =
+                element.getAttribute("data-en");
+
+        } else {
+
+            element.textContent =
+                element.getAttribute("data-ar");
+
+        }
+
+    });
+
+
+    /* =========================
+       ABOUT US
+    ========================= */
+
+    const aboutAr = document.querySelector(".about-ar");
+    const aboutEn = document.querySelector(".about-en");
+
+    if (aboutAr && aboutEn) {
+
+        if (currentLanguage === "en") {
+
+            aboutAr.style.display = "none";
+            aboutEn.style.display = "block";
+
+        } else {
+
+            aboutAr.style.display = "block";
+            aboutEn.style.display = "none";
+
+        }
+
+    }
+
+
+    /* =========================
+       PAGE LANGUAGE
+    ========================= */
+
+    if (currentLanguage === "en") {
+
+        document.documentElement.lang = "en";
+        document.documentElement.dir = "ltr";
+
+        languageBtn.textContent = "العربية";
+
+    } else {
+
+        document.documentElement.lang = "ar";
+        document.documentElement.dir = "rtl";
+
+        languageBtn.textContent = "English";
+
+    }
+
+}
+
+
+/* =========================
+   LANGUAGE BUTTON
+========================= */
+
 if (languageBtn) {
 
     languageBtn.addEventListener("click", function () {
@@ -14,40 +91,13 @@ if (languageBtn) {
 
             currentLanguage = "en";
 
-            document.documentElement.lang = "en";
-            document.documentElement.dir = "ltr";
-
-            languageBtn.textContent = "العربية";
-
         } else {
 
             currentLanguage = "ar";
 
-            document.documentElement.lang = "ar";
-            document.documentElement.dir = "rtl";
-
-            languageBtn.textContent = "English";
-
         }
 
-        const elements =
-            document.querySelectorAll("[data-ar][data-en]");
-
-        elements.forEach(function (element) {
-
-            if (currentLanguage === "en") {
-
-                element.textContent =
-                    element.getAttribute("data-en");
-
-            } else {
-
-                element.textContent =
-                    element.getAttribute("data-ar");
-
-            }
-
-        });
+        updateLanguage();
 
     });
 
